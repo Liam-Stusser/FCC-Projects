49299c03ff51d09581f23b8d82a54a173a42edc8
Liam Stusser <liamstusser@gmail.com>
2025-10-27 21:04:44 -0700

    fix: Edited to pass fcc test

--- BackendApis/project5.js
+++ BackendApis/project5.js
@@ -1,35 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const upload = multer({storage: multer.memoryStorage()});
+const upload = multer();
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
-  }
+  if (!req.file) return res.json({ error: 'No file uploaded' });
 
   const { originalname, mimetype, size } = req.file;
-
-  res.json({
-    name: originalname,
-    type: mimetype,
-    size: size
-  });
+  res.json({ name: originalname, type: mimetype, size });
 });
 
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log('Your app is listening on port ' + port);
 });
